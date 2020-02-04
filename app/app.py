41d7c94c61ffd47f8f4cebded283a4e0f9a95e87
Yermakov Kirill <yermakov.kirill@gmail.com>
2020-02-04 14:52:15 +0300

    Доработал тесты и подключил Travis CI.

--- app/app.py
+++ app/app.py
@@ -1,42 +1,127 @@
 # coding: utf-8
 
 import os
-from werkzeug.datastructures import FileStorage
-from flask import Flask, request, redirect, jsonify, Response
+import re
+from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
+import simplejson as json
+import urllib
+import base64
+import uuid
+from io import BytesIO
+from PIL import Image
+from mimetypes import guess_extension, guess_type
 
 UPLOAD_FOLDER = 'images'
+THUMBS_FOLDER = 'images/thumbs'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['THUMBS_FOLDER'] = THUMBS_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 
-def allowed_file(filename):
+def allowed_file(filename: str):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-def validate_requested_file(file: FileStorage) -> int:
-    if file.filename == '':
-        return 400
-    if file and allowed_file(file.filename):
-        return 201
+def is_valid_url(filename: str) -> bool:
+    try:
+        urllib.request.urlopen(filename)
+    except IOError:
+        return False
+    return True
+
+
+def create_thumbnail(filepath):
+    img = Image.open(filepath)
+    img_resized = img.resize((100, 100))
+    filename = get_random_filename() + '.' + filepath.rsplit('.', 1)[1].lower()
+    img_resized.save(os.path.join(app.config['THUMBS_FOLDER'], filename))
+
+
+def validate_filename(filename, content_type: str) -> int:
+    if request.content_type == 'application/json':
+        if is_base64(filename):
+            return 201
+        if is_valid_url(filename) and allowed_file(filename):
+            return 201
+        else:
+            return 400
     else:
-        return 400
+        if filename == '':
+            return 400
+        if allowed_file(filename.filename):
+            return 201
+        else:
+            return 400
 
 
-def save_file(file: FileStorage):
-    filename = secure_filename(file.filename)
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+
+def save_files(uploaded_files: list, content_type: str):
+    filepath = ''
+    if content_type.startswith('application/json'):
+        for resource in uploaded_files:
+            if is_valid_url(resource) and allowed_file(resource):
+                filepath = save_from_url(resource)
+            if is_base64(resource):
+                filepath = save_base64(resource)
+            create_thumbnail(filepath)
+    else:
+        for file in uploaded_files:
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
+            create_thumbnail(filepath)
+
+
+def save_from_url(url: str) -> str:
+    filename = re.search('.*/(.*)', url).group(1)
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    urllib.request.urlretrieve(url, filepath)
+    return filepath
+
+
+def get_random_filename() -> str:
+    return str(uuid.uuid4())
+
+
+def save_base64(res: str) -> str:
+    file_ext = guess_extension(guess_type(res)[0])
+    filename = get_random_filename() + file_ext
+
+    starter = res.find(',')
+    image_data = res[starter + 1:]
+    image_data = bytes(image_data, encoding="ascii")
+    im = Image.open(BytesIO(base64.b64decode(image_data)))
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    im.save(filepath)
+    return filepath
 
 
 def is_valid_content_type(content_type: str) -> bool:
-    if content_type.startswith('application/json') or content_type.startswith('multipart/form_data'):
+    if content_type.startswith('application/json') or content_type.startswith('multipart/form-data'):
         return True
 
     return False
+
+
+def is_base64(s: str) -> bool:
+    starter = s.find(',')
+    image_data = s[starter + 1:]
+    image_data = bytes(image_data, encoding="ascii")
+    try:
+        return base64.b64encode(base64.b64decode(image_data)) == image_data
+    except Exception:
+        return False
 
 
 @app.route('/upload', methods=['POST'])
@@ -46,22 +131,32 @@ def upload_file():
         resp.status_code = 400
         return resp
 
-    uploaded_files = request.files.getlist("file")
+    if request.content_type == 'application/json':
+        try:
+            uploaded_files = json.loads(request.data)
+        except Exception as e:
+            raise e
+    else:
+        uploaded_files = request.files.getlist("file")
 
-    for file in uploaded_files:
-        status_code = validate_requested_file(file)
+    for filename in uploaded_files:
+        status_code = validate_filename(filename, request.content_type)
         if status_code != 201:
             resp = jsonify({'message': 'Allowed file types are png, jpg, jpeg'})
             resp.status_code = status_code
             return resp
 
-    for file in uploaded_files:
-        save_file(file)
-
+    save_files(uploaded_files, request.content_type)
     resp = jsonify({'message': 'File(s) successfully uploaded'})
     resp.status_code = 201
 
     return resp
+
+
+@app.route('/shutdown', methods=['GET'])
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
 
 
 if __name__ == "__main__":
--- app/exceptions.py
+++
@@ -1,3 +0,0 @@
-class InvalidContentTypeException(Exception):
-
-    pass
